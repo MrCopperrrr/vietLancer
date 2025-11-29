955f78f8641bb97ef616240efe5f8c30053b0cc2
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2025-11-29 16:51:42 +0700

    fix cors

--- webserver/routes/app.js
+++ webserver/routes/app.js
@@ -23,7 +23,16 @@ const app = express();
 app.use(serveFavicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",             // Cho phép chạy Local ở nhà
+        "http://localhost:3000",             // Cho phép Backend Local
+        "https://vietlancerbackend.onrender.com", // Link chính Backend
+        "https://vietlancerweb.onrender.com"     // 
+    ],
+    credentials: true, // Quan trọng: Để cho phép gửi token/cookie đăng nhập
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] // Cho phép các method này
+}));
 app.use('/api', uploadRoute);
 app.use('/api', signUpRouter);
 app.use('/api', signInRouter);
